6dfd0597dc11061ae02b40657e8f8d9275cef704
luisamrv <luisa.rodriguesv@gmail.com>
2025-08-03 12:55:11 +0100

    created tables, migrations, relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,12 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $appends = [
         'profile_photo_url',
     ];
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
 }
