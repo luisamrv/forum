5e5fff361bc48a1e5896c7c91fdaf3756edc00cf
luisamrv <luisa.rodriguesv@gmail.com>
2025-08-03 15:07:23 +0100

    factories and seeding

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -3,6 +3,9 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Post;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
@@ -17,7 +20,9 @@ class CommentFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'post_id' => Post::factory(),
+            'body' => fake()->realText(250)
         ];
     }
 }
